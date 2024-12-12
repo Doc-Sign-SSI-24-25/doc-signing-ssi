57c212118da6e5c7b3a475c1f94b50092b4eb6ae
Alvaro Eduardo Menegon Rosario <m319998@alunos.ipb.pt>
2024-12-12 15:32:18 +0000

    padronização do algoritmo de hash

--- app/utils/crypto_utils.py
+++ app/utils/crypto_utils.py
@@ -1,28 +1,26 @@
 import hashlib
 
-def create_hash(input_file: bytes, algorithm: str = "sha256") -> str:
+def create_hash(input_file: bytes) -> str:
     """
     Cria um hash a partir de um arquivo em bytes.
 
     :param input_file: O conteúdo do arquivo em bytes.
-    :param algorithm: O algoritmo de hash a ser utilizado (padrão: 'sha256').
+    O algoritmo de hash a ser utilizado é 'sha256').
     :return: O hash gerado em formato hexadecimal.
     """
-    try:
-        hash_func = hashlib.new(algorithm)
-        hash_func.update(input_file)
-        return hash_func.hexdigest()
-    except ValueError:
-        raise ValueError(f"Algoritmo de hash '{algorithm}' não é suportado.")
+    hash_func = hashlib.new("sha256")
+    hash_func.update(input_file)
+    return hash_func.hexdigest()
 
-def verify_hash(input_file: bytes, hash: str, algorithm: str = "sha256") -> bool:
+
+def verify_hash(input_file: bytes, hash: str) -> bool:
     """
     Verifica se o hash gerado a partir do arquivo em bytes corresponde ao hash esperado.
 
     :param input_file: O conteúdo do arquivo em bytes.
     :param hash: O hash esperado para comparação.
-    :param algorithm: O algoritmo de hash a ser utilizado (padrão: 'sha256').
+    O algoritmo de hash a ser utilizado é 'sha256').
     :return: True se os hashes corresponderem, False caso contrário.
     """
-    hash_gerado = create_hash(input_file, algorithm)
+    hash_gerado = create_hash(input_file)
     return hash_gerado == hash
