eb83c2fc4f7f81ae55ac005cd09af97097aa2e9a
Alvaro Eduardo Menegon Rosario <m319998@alunos.ipb.pt>
2024-12-12 14:47:31 +0000

    create hash functions

--- app/controllers/document_controller.py
+++ app/controllers/document_controller.py
@@ -20,10 +20,12 @@ class DocumentController(BaseController):
             raise HTTPException(status_code=400, detail="Certificado não encontrado")
         signer = Signer(name=user["name"], email=user["email"], private_key=user["private_key"], cert_pem=user["certificate"])
         try:
+            from app.utils.crypto_utils import create_hash
             signed_document = sign_pdf(request.file_content, signer,request.reason, request.location, request.positions)
+            signed_document_hash = create_hash(signed_document)
             import base64
             file_base_64 = base64.b64encode(signed_document).decode('utf-8')
-            return {"signed_document": file_base_64, "filename": request.filename.replace(".pdf", "-signed.pdf")}
+            return {"signed_document": file_base_64, "filename": request.filename.replace(".pdf", "-signed.pdf"), "hash": signed_document_hash}
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f'Erro ao assinar: {e}')
--- app/utils/crypto_utils.py
+++ app/utils/crypto_utils.py
@@ -1,7 +1,28 @@
-import bcrypt
+import hashlib
 
-def hashPassword(password: str) -> str:
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+def create_hash(input_file: bytes, algorithm: str = "sha256") -> str:
+    """
+    Cria um hash a partir de um arquivo em bytes.
 
-def checkPassword(password: str, hashed: str) -> bool:
-    return bcrypt.checkpw(password.encode(), hashed.encode())
+    :param input_file: O conteúdo do arquivo em bytes.
+    :param algorithm: O algoritmo de hash a ser utilizado (padrão: 'sha256').
+    :return: O hash gerado em formato hexadecimal.
+    """
+    try:
+        hash_func = hashlib.new(algorithm)
+        hash_func.update(input_file)
+        return hash_func.hexdigest()
+    except ValueError:
+        raise ValueError(f"Algoritmo de hash '{algorithm}' não é suportado.")
+
+def verify_hash(input_file: bytes, hash: str, algorithm: str = "sha256") -> bool:
+    """
+    Verifica se o hash gerado a partir do arquivo em bytes corresponde ao hash esperado.
+
+    :param input_file: O conteúdo do arquivo em bytes.
+    :param hash: O hash esperado para comparação.
+    :param algorithm: O algoritmo de hash a ser utilizado (padrão: 'sha256').
+    :return: True se os hashes corresponderem, False caso contrário.
+    """
+    hash_gerado = create_hash(input_file, algorithm)
+    return hash_gerado == hash
