45b82a1e356d3378539d3631f2412b7433a4def1
Alvaro Eduardo Menegon Rosario <m319998@alunos.ipb.pt>
2024-12-12 15:29:48 +0000

    remover arquivos para testar na linha de comando

--- sign_pdf.py
+++
@@ -1,9 +0,0 @@
-from app.models.signer import Signer
-from app.utils.cli import import_file,export_file,import_cert,import_key
-    
-key = import_key('app/static/john-key.pem')
-cert = import_cert('app/static/john-cert.pem')
-signer = Signer(name="John Doe", email="johndoe@mail.com", private_key=key,cert_pem=cert)
-file = import_file('app/static/pdf.pdf')
-signed = file.sign(signer=signer, reason="reason",location= "location")
-export_file(signed, f'{signed.name}')
